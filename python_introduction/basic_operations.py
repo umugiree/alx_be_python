7cfa3326c1e9354f68a60d48c32a7f5a46be1e80
Emmanuel UMUGIRE <umugire@aol.com>
2025-07-13 19:49:00 +0200

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define 2 numbers:
+number1 = 10
+number2 = 5
+
+# Perform Operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# printout the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
