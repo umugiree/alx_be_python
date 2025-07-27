cb17740693a646228fc15fa7ed2bedae83a5a1a6
Emmanuel UMUGIRE <umugire@aol.com>
2025-07-27 22:36:31 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,16 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    operation = operation.lower()
+
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
 
