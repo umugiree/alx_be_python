ba680585b3043b2df821a183218aa0d3a12f63ed
Emmanuel UMUGIRE <umugire@aol.com>
2025-07-27 22:38:18 +0200

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     operation = operation.lower()
 
     if operation == 'add':
@@ -13,4 +13,3 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "Error: Invalid operation"
-
